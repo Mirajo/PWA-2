7438d6b7735613534177f50e7f1c92bf60803a21
Mirajo <mmj_chicago@yahoo.com>
2014-08-15 23:22:40 -0400

    css updated
    css update model code removed

--- WK1/Johnson_Miranda_Project/js/main.js
+++ WK1/Johnson_Miranda_Project/js/main.js
@@ -4,15 +4,13 @@
 	PWAII
 */
 
-(function($){
+(function($) {
 	
 	
 	/*
-	===============================================
-	========================= APPLICATION FUNCTIONS	
+	======================== APPLICATION FUNCTIONS =====================
 	*/
-	
-	
+
 	var checkLoginState = function(){
 		$.ajax({
 			url: 'xhr/check_login.php',
@@ -24,8 +22,37 @@
 			}
 		});
 	};
-	
-	
+
+    /*
+     ========================= MODAL ========================
+     */
+    $('.modalClick').on('click', function(event){
+        event.preventDefault();                                      //prevent default page use modal
+         $('#overlay')                                               //call out the overlay
+             .fadeIn()                                               // Fade in the overlay
+             .find('#modal')                                         //find the modal
+             .fadeIn();                                              //fade in the modal
+    });
+
+    $('.close').on('click', function (event){                        //close modal on click
+        event.preventDefault();                                      //prevent default page use modal
+        $('#overlay')                                                //call out overlay
+            .fadeOut()                                               //Fade out overlay
+            .find('#modal')                                          //find modal
+            .fadeOut();                                              //fade out modal
+
+    });
+
+    /*
+     ====================== FADING STATUS OPTION =======================
+     */
+    $('.status').mouseover(function(){                                //mover over status icons
+        $(this).fadeTo(100,.3);                                       //fade icons during mouse over
+     });
+
+    $('.status').mouseout(function(){                                 //mouse over status icons
+        $(this).fadeTo(100,1);                                        //fade icons during mouse over
+     });
 
 	// 	============================================
 	//	SETUP FOR INIT
